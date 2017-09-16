27148fbf027492cd3023b4cf1f3552375f2f5892
Orr Barkat <barkat.orr@gmail.com>
2017-09-16 21:38:28 +0300

    updates

--- js/content/banThem.js
+++ js/content/banThem.js
@@ -1,37 +1,32 @@
 console.log("HELLO");
 banArtists();
-setInterval(banArtists, 60000);
+setInterval(banArtists, 2000);
 console.log("DOG");
 
 
 function banArtists(){
   chrome.storage.sync.get("artists", function(data){
-    if(!data){
+    if(!data || !data.artists || !data.artists.length){
       console.log("no artists in the list");
     }else{
       let list = data.artists;
-      let songs = document.getElementsByClassName("yt-uix-scroller-scroll-unit");
-      for(let i=0; i<songs.length; i++){
-        checkAndDelete(songs[i],list);
-      }
+      let currentSong = document.getElementById("eow-title");
+      checkAndDelete(currentSong,list);
     }
   });
 }
 
-function checkAndDelete(element, artistList){
+function checkAndDelete(currentSong, artistList){
   if (!artistList.length){
     return NULL;
   }else{
+    let title = currentSong.getAttribute("title").toLowerCase();
     for(var i=0; i<artistList.length; i++) {
-      if(element.dataset.videoTitle.toLowerCase().includes(artistList[i])){
-        // element.children[1].children[0].click();
-        if(element.className.includes("currently-playing")){
-          element.nextElementSibling.children[1].click();
-        }else{
-          element.parentNode.removeChild(element);
-        }
-        console.log("removed: " + element.dataset.videoTitle + "from your playlist")
-        break;
+      if(title.includes(artistList[i].toLowerCase())){
+        let next = document.getElementsByClassName("ytp-next-button ytp-button")[0];
+        next.click();
+        console.log("removed: " + currentSong.getAttribute("title") + "from your playlist")
+        return;
       }
     }
   }
